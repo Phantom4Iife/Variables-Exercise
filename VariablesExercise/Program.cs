d73c18a32f4dafa104646784e5607697d5cd3612
Jeremiah Augustin <Phantom4iife7@gmail.com>
2025-02-10 16:05:48 -0500

    Finished Project

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string name = "Jeremiah";
+            int number = 7;
+            char letter = 'A';
+            bool prob = true;
+            double number2 = 2.4;
+            decimal number3 = 2.916666667m;
+                Console.WriteLine($"{letter} student named {name} knows that {number} divided by {number2} equals {number3} is {prob}");
         }
     }
 }
